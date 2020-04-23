915fd7f9f308803c902de34301a00bfa1a3f8efe
Giacomo Colizzi Coin <colizzicoin.1794538@studenti.uniroma1.it>
2020-04-23 19:28:11 +0200

    users_controller; user;index,new,_form,show(just created),create for users

--- source/game_on/db/schema.rb
+++ source/game_on/db/schema.rb
@@ -39,9 +39,12 @@ ActiveRecord::Schema.define(version: 2020_04_23_113439) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
