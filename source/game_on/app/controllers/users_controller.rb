035a037739d14dd1484dea0c6210a3fb600a4a02
Giacomo Colizzi Coin <colizzicoin.1794538@studenti.uniroma1.it>
2020-04-23 16:29:47 +0200

    modifiche temporanee per me

--- source/game_on/app/controllers/users_controller.rb
+++ source/game_on/app/controllers/users_controller.rb
@@ -5,10 +5,26 @@ class UsersController < ApplicationController
     end
   
     def show
-      render html: 'users show'
     end
 
     def new
-      
+    end
+
+    def create
+      @user = User.new(user_params)
+
+      if @user.save
+        flash[:notice] = "User #{@user.username} was added"
+        redirect_to users_path
+      else
+        flash[:notice] = @user.errors.full_messages
+        redirect_to users_path
+    end
+
+    private
+
+      def user_params
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
+      end
     end
   end
