f97b979c3d5d94d5500b23cc6fa653f0266a16a9
Giacomo Colizzi Coin <colizzicoin.1794538@studenti.uniroma1.it>
2020-06-10 12:22:38 +0200

    corrected update for omniauth users

--- source/game_on/app/controllers/users/registrations_controller.rb
+++ source/game_on/app/controllers/users/registrations_controller.rb
@@ -2,7 +2,8 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  layout "devise"
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -20,9 +21,24 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    @user = User.find(current_user.id)
+    email_changed = @user.email != params[:user][:email]
+    is_oauth_account = !@user.provider.blank?
+
+    successfully_updated = if !is_oauth_account
+      @user.update_with_password account_update_params
+    else
+      @user.update_without_password account_update_params.except(:current_password)
+    end
+
+    if successfully_updated
+      bypass_sign_in @user
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
 
   # DELETE /resource
   # def destroy
@@ -46,9 +62,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
