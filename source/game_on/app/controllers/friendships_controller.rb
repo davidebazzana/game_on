5ae0e0ec3c62f2068d90287b2e922910f365b031
davidebazzana <db.bazzana@gmail.com>
2020-07-11 22:57:34 +0200

    Modified warning messages and eliminated redundancies

--- source/game_on/app/controllers/friendships_controller.rb
+++ source/game_on/app/controllers/friendships_controller.rb
@@ -1,19 +1,19 @@
 class FriendshipsController < ApplicationController
     def create
-        @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
-        if @friendship.save
-          flash[:notice] = "Added follower"
-          redirect_to root_url
-        else
-          flash[:error] = "Unable to add follower"
-          redirect_to root_url
-        end
+      @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+      if @friendship.save
+        flash[:notice] = "Following #{User.find(@friendship.friend_id).username}"
+        redirect_to root_url
+      else
+        flash[:error] = "Unable to follow"
+        redirect_to root_url
       end
-      
-      def destroy
-        @friendship = current_user.friendships.find(params[:id])
-        @friendship.destroy
-        flash[:notice] = "Removed follower"
-        redirect_to current_user
-      end
+    end
+    
+    def destroy
+      @friendship = current_user.friendships.find(params[:id])
+      @friendship.destroy
+      flash[:notice] = "Not following #{User.find(@friendship.friend_id).username} anymore"
+      redirect_to current_user
+    end
 end
