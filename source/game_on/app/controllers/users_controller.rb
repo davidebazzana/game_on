5e233723413b62acb7514d4852adf72b0dfc17ae
renatogiamba <you@example.com>
2020-08-30 14:01:03 +0200

    Follow online status

--- source/game_on/app/controllers/users_controller.rb
+++ source/game_on/app/controllers/users_controller.rb
@@ -2,10 +2,12 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @online_users = User.where("last_seen_at > ?", 2.minutes.ago)
   end
 
   def show
     @user = User.find(params[:id])
+    @online_users = User.where("last_seen_at > ?", 5.minutes.ago)
     render :layout => "devise"
   end
   
--- source/game_on/db/schema.rb
+++ source/game_on/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_21_165857) do
+ActiveRecord::Schema.define(version: 2020_08_30_095744) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -47,6 +47,32 @@ ActiveRecord::Schema.define(version: 2020_06_21_165857) do
     t.index ["user_id"], name: "index_games_on_user_id"
   end
 
+  create_table "invitations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "invitefriends", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "invites", force: :cascade do |t|
+    t.string "email"
+    t.string "token"
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.integer "invitable_id"
+    t.string "invitable_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_invites_on_email"
+    t.index ["invitable_id", "invitable_type"], name: "index_invites_on_invitable_id_and_invitable_type"
+    t.index ["recipient_id"], name: "index_invites_on_recipient_id"
+    t.index ["sender_id"], name: "index_invites_on_sender_id"
+    t.index ["token"], name: "index_invites_on_token"
+  end
+
   create_table "reviews", force: :cascade do |t|
     t.text "comments"
     t.integer "user_id"
@@ -66,6 +92,7 @@ ActiveRecord::Schema.define(version: 2020_06_21_165857) do
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
+    t.datetime "last_seen_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
