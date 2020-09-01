70988ff9d9b87d73a079c41c3407551173e99e3b
renatogiamba <you@example.com>
2020-09-01 14:18:15 +0200

    eliminati link inutili

--- source/game_on/config/environments/development.rb
+++ source/game_on/config/environments/development.rb
@@ -35,6 +35,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  ActionMailer::Base.default :from => 'Game_on@noreplay.com'
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.delivery_method = :smtp
