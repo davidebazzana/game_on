43f6aff630149c43a02db4dff9489743216eda83
Giacomo Colizzi Coin <6ninjak@gmail.com>
2020-09-02 23:05:00 +0200

    Updated client id and secret for google oauth

--- source/game_on/config/initializers/devise.rb
+++ source/game_on/config/initializers/devise.rb
@@ -259,7 +259,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :google_oauth2, '292662274025-2m9vnd1hiappkqhjqfqshalg3o3cd28f.apps.googleusercontent.com', 'RtisuQ1yE29y1SwsGoiNCEXf', skip_jwt: true
+  config.omniauth :google_oauth2, '222655653205-fjqirncvn9grk7e3gpmmrgpo335cioe4.apps.googleusercontent.com', '4vTrYQd8uJlj2s9znpmvN1fk', skip_jwt: true
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
