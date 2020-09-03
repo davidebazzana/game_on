522541571a8fc3d39c8e5684849d7bc42a3145d0
Giacomo Colizzi Coin <6ninjak@gmail.com>
2020-09-03 13:15:46 +0200

    Fix: Now a user can sign up using a provider and then signing in using another one that carries the same email

--- source/game_on/app/models/user.rb
+++ source/game_on/app/models/user.rb
@@ -22,7 +22,7 @@ class User < ApplicationRecord
   end
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    where(provider: auth.provider, uid: auth.uid).or(where(email: auth.info.email)).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.username = auth.info.name
