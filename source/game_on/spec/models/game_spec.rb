e0c30b890faf2129dddd6c8295e93c73244c9f95
davidebazzana <db.bazzana@gmail.com>
2020-09-08 10:58:47 +0200

    Added two more tests to 'game_spec.rb'

--- source/game_on/spec/models/game_spec.rb
+++ source/game_on/spec/models/game_spec.rb
@@ -2,20 +2,20 @@ require 'spec_helper'
 
 RSpec.describe Game, type: :model do
   before(:all) {
-    titles = ["Pet 'em all",
-              "Cats island",
-              "Dogcatcher simulator",
-              "Sleeping cat"]
+    games = [{:title => "Pet 'em all", :category => "Other"},
+              {:title => "Cats island", :category => "Other"},
+              {:title => "Dogcatcher simulator", :category => "Other"},
+              {:title => "Sleeping cat", :category => "Other"},
+              {:title => "Cat race", :category => "Racing"},
+              {:title => "Purring cat", :category => "Casual"},
+              {:title => "Mreow", :category => "Casual"}]
+              
     
     FactoryBot.create(:user)
     FactoryBot.create(:game)
-    titles.each do |title|
-      FactoryBot.create(:game, :title => title)
+    games.each do |game|
+      FactoryBot.create(:game, :title => game[:title], :category => game[:category])
     end
-  }
-
-  after(:all) {
-    DatabaseCleaner.clean
   }
   
   it "Search for all the games with 'cat' in their name" do
@@ -23,6 +23,22 @@ RSpec.describe Game, type: :model do
     search_result = Game.search(search_game_params)
     res = Array.new()
     search_result.each{|entry| res.append(entry[:title])}
-    expect(res).to eq(["Cats island", "Dogcatcher simulator", "Sleeping cat"])
+    expect(res).to eq(["Cats island", "Dogcatcher simulator", "Sleeping cat", "Cat race", "Purring cat"])
+  end
+
+  it "Search for all the games with category 'Casual'" do
+    search_game_params = {:title => "", :category => "Casual", :sorting_criterion => "Any"}
+    search_result = Game.search(search_game_params)
+    res = Array.new()
+    search_result.each{|entry| res.append(entry[:title])}
+    expect(res).to eq(["Purring cat", "Mreow"])
+  end
+
+  it "Search for all the games with 'cat' in their name and category 'Casual'" do
+    search_game_params = {:title => "cat", :category => "Casual", :sorting_criterion => "Any"}
+    search_result = Game.search(search_game_params)
+    res = Array.new()
+    search_result.each{|entry| res.append(entry[:title])}
+    expect(res).to eq(["Purring cat"])
   end
 end
