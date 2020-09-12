195984ed48453d7943fdccc38238a00d65638e2d
Giacomo Colizzi Coin <6ninjak@gmail.com>
2020-09-12 13:00:46 +0200

    email confirmation implemented

--- source/game_on/config/routes.rb
+++ source/game_on/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
+    confirmations: 'users/confirmations',
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
   devise_scope :user do
